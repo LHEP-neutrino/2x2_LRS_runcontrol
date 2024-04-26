50dc761718bc7f10c0e9a9721988b7e10e0f630f
liviocali <liviocalivers@hotmail.com>
2024-04-26 11:50:53 -0500

    Update version number for stable release

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="lrsctrl",
-    version="1.0.0",
+    version="2.0.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
@@ -11,6 +11,7 @@ setup(
     },
     description="2x2 Light Readout DAQ Run Control",
     author="Livio Calivers; Greenlab",
+    author_email="livio.calivers@unibe.ch",
     classifiers=["Intended Audience :: Information Technology",
                  "Operating System :: POSIX :: Linux",
                  "Programming Language :: Python",
