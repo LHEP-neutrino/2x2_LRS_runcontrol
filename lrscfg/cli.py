d2d65e5e410b43b538dcf8254d5ab0c16d3d099f
liviocali <liviocalivers@hotmail.com>
2024-04-30 18:27:33 -0500

    Add database functionality to lrscfg

--- lrscfg/cli.py
+++ lrscfg/cli.py
@@ -7,5 +7,21 @@ def lrscfg():
 
 #Data run controls
 @lrscfg.command()
-def pull_moas():
-    Client().pull_moas()
+@click.option("--tag","-t", required=True, type=str, help="Description tag")
+def pull_moas(tag):
+    Client().pull_moas(tag)
+
+@lrscfg.command()
+def get_latest_moas():
+    print(Client().get_latest_moas())
+    return Client().get_latest_moas()
+
+@lrscfg.command()
+def get_active_moas():
+    print(Client().get_active_moas())
+    return Client().get_active_moas()
+
+@lrscfg.command()
+@click.option("--version","-v", required=True, type=str, help="MOAS version tag ()")
+def set_active_moas(version):
+    Client().set_active_moas(version)
--- lrscfg/client.py
+++ lrscfg/client.py
@@ -1,5 +1,6 @@
 import requests
 from lrsctrl.config import Config
+from lrscfg.db_handler import DB_Handler
 from datetime import datetime
 import json
 import filecmp, os, glob
@@ -10,10 +11,13 @@ class Client():
         config_settings = Config().parse_yaml()
         self.moas_path = config_settings["moas_path"]+"/"
         self.latest_moas = self.get_latest_moas()
+        self.db = DB_Handler()
 
-    def pull_moas(self):
+    def pull_moas(self, tag):
         my_date = datetime.now()
-        file_name = my_date.strftime("MOAS_%Y_%m_%d_%H_%M_%S.csv")
+
+        #pull and save csv
+        file_name = my_date.strftime("MOAS_%Y%m%d_%H%M%S.csv")
         file_path = self.moas_path + file_name
         pull_command = "wget 'https://docs.google.com/spreadsheets/d/13CcIy80dR0nSdZkhVfw3O8aI-YYRBjdlX6SshXjmRLM/export?format=csv&gid=790353874' -O '" + file_path + "' &> /dev/null"
         os.system(pull_command)
@@ -23,6 +27,9 @@ class Client():
                 print("Current MOAS identical to previous version, no new version created")
                 return self.latest_moas
         self.latest_moas = file_name
+
+        #push to db
+        self.db.import_configuration(file_path, tag)
         return file_name
     
     def get_latest_moas(self):
@@ -31,5 +38,15 @@ class Client():
         if not files:
             print("No previous MOAS found")
             return None
-        latest_file = max(files, key=os.path.getctime)
+        latest_file = max(files, key=os.path.getmtime)
         return os.path.basename(latest_file)
+
+    def get_active_moas(self):
+        version = self.db.get_active_configuration()
+        if not version:
+            print("No active MOAS set")
+            return None
+        return "MOAS_" + version + ".csv"
+
+    def set_active_moas(self, version):
+        self.db.update_active_configuration(version)
